33276b1ee9c2ea4bd64dfeaeefa2c7b543814fc2
Fernando Salamanca <lfernandosalamanca@gmail.com>
2021-12-01 22:05:09 +0000

    added user and character routes plus test db

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import json
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +31,134 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# Fills DB with database.json test database. RUN THIS FIRST SO YOU CAN TEST THE ROUTES! 
+@app.route("/fill-db", methods=['GET'])
+def fill_database():
+    f = open("src/database.json", "r")
+    content = f.read()
+    jsondecoded = json.loads(content)
+
+    for character in jsondecoded['characters']:
+        new_character = Character(name = character['name'], height = character['height'],
+        mass =character['mass'], hair_color = character['hair_color'], skin_color = character['skin_color'],
+        eye_color = character['eye_color'], birth_year = character['birth_year'], gender = character['gender'],
+        url = character['url'])
+        db.session.add(new_character)
+
+
+    for planet in jsondecoded['planets']:
+        new_planet = Planet(name = planet['name'], diameter = planet['diameter'], rotation_period = planet['rotation_period'],
+        orbital_period = planet['orbital_period'], gravity = planet['gravity'], population =planet['population'],
+        climate = planet['climate'], terrain = planet['terrain'], surface_water = planet['surface_water'], url = planet['url'])
+        db.session.add(new_planet)
+
+    db.session.commit()
+
+    return jsonify({"msg": "Database filled!"}), 200
+
+# USER ROUTES
+@app.route('/get-users', methods=['GET'])
+def get_users():
+    users = User.get_all_users()
+
+    users = list(map(lambda user: user.serialize(), users))
+    return jsonify(users), 200
+
+@app.route('/add-user', methods=['POST'])
+def add_user():
+    name = request.json.get('name')
+    email = request.json.get('email')
+    password = request.json.get('password')
+    user = User(name=name, email=email, password=password)
+
+    User.save(user)
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "User added sussesfully",
+        "user": user.name
     }
 
     return jsonify(response_body), 200
+
+# CHARACTER ROUTES
+@app.route('/get-characters', methods=['GET'])
+def get_characters():
+    characters = Character.get_all_characters()
+
+    characters = list(map(lambda character: character.serialize(), characters))
+    return jsonify(characters), 200
+
+@app.route('/get-character/<int:character_id>', methods=['GET'])
+def get_character_by_id(character_id):
+    character = Character.get_character_by_id(character_id)
+
+    response_body = {
+        "msg": "Character found.",
+        "character": character.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/add-character', methods=['POST'])
+def add_character():
+    name = request.json.get('name')
+    height = request.json.get('height')
+    mass = request.json.get('mass')
+    hair_color = request.json.get('hair_color')
+    skin_color = request.json.get('skin_color')
+    eye_color = request.json.get('eye_color')
+    birth_year = request.json.get('birth_year')
+    gender = request.json.get('gender')
+    url = request.json.get('url')
+    character = Character(name=name, height=height, mass=mass, hair_color=hair_color,
+    skin_color=skin_color, eye_color=eye_color, birth_year=birth_year, gender=gender,
+    url=url)
+
+    Character.save(character)
+
+    response_body = {
+        "msg": "Character added sussesfully",
+        "character": character.name
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/edit-character/<int:character_id>', methods=['PUT'])
+def edit_character(character_id):
+    character = Character.get_character_by_id(character_id)
+
+    name = request.json.get('name')
+    height = request.json.get('height')
+    mass = request.json.get('mass')
+    hair_color = request.json.get('hair_color')
+    skin_color = request.json.get('skin_color')
+    eye_color = request.json.get('eye_color')
+    birth_year = request.json.get('birth_year')
+    gender = request.json.get('gender')
+    url = request.json.get('url')
+
+    if name is None:
+        return jsonify({"msg": "Name is mandatory"})
+
+    character.name = name
+    character.height = height
+    character.mass = mass
+    character.hair_color = hair_color
+    character.skin_color = skin_color
+    character.eye_color = eye_color
+    character.birth_year = birth_year
+    character.gender = gender
+    character.url = url
+
+    Character.commit()
+
+    response_body = {
+        "msg": "Character updated sussesfully",
+        "character": character.name
+    }
+
+    return jsonify(response_body), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,11 +2,28 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class GeneralModel: 
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def commit ():
+        db.session.commit()
+
+    def add (self):
+        db.session.add(self)
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class User(db.Model, GeneralModel):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorite', lazy=True)
+    
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +31,97 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "email": self.email, 
+            "favorites": self.favorites  
         }
+
+    def get_user_by_id(id):
+        return User.query.filter_by(id=id).first()
+
+    def get_all_users():
+        return User.query.all()
+
+class Character(db.Model, GeneralModel):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)    
+    height = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    hair_color = db.Column(db.String(250))
+    skin_color = db.Column(db.String(250))
+    eye_color = db.Column(db.String(250))
+    birth_year = db.Column(db.String(250))
+    gender = db.Column(db.String(250))
+    url = db.Column(db.String(250))
+    favorites = db.relationship('Favorite', lazy=True)
+
+    def __repr__(self):
+        return '<Character %r>' % self.charactername
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,   
+            "mass": self.mass,   
+            "hair_color": self.hair_color,   
+            "skin_color": self.skin_color,   
+            "eye_color": self.eye_color,   
+            "birth_year": self.birth_year,   
+            "gender": self.gender,   
+            "url": self.url,   
+        }
+    def get_character_by_id(id):
+        return Character.query.filter_by(id=id).first()
+
+    def get_all_characters():
+        return Character.query.all()
+
+class Planet(db.Model, GeneralModel):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)  
+    diameter = db.Column(db.Integer)
+    rotation_period = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    gravity = db.Column(db.String(250))
+    population = db.Column(db.Integer)
+    climate = db.Column(db.String(250))
+    terrain = db.Column(db.String(250))
+    surface_water = db.Column(db.Integer)
+    url = db.Column(db.String(250))
+    favorites = db.relationship('Favorite', lazy=True)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.planetname
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,   
+            "rotation_period": self.rotation_period,   
+            "orbital_period": self.orbital_period,   
+            "gravity": self.gravity,   
+            "population": self.population,   
+            "climate": self.climate,   
+            "terrain": self.terrain,   
+            "surface_water": self.surface_water,   
+            "url": self.url,   
+        }
+    def get_character_by_id(id):
+        return Character.query.filter_by(id=id).first()
+
+    def get_all_characters():
+        return Character.query.all()
+
+class Favorite(db.Model, GeneralModel):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    character = db.relationship(Character)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    planet = db.relationship(Planet)
+
+    def to_dict(self):
+        return {}
